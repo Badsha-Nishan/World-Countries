4a8feba2b18daca9a6f46085801d030b2f29ae82
Badsha-Nishan <badshanisan14@gmail.com>
2026-03-15 16:36:10 +0600

    added total length of total visited country

--- src/Countries/Countries.jsx
+++ src/Countries/Countries.jsx
@@ -1,17 +1,28 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from "./Country";
-import "./Countries.css"
+import "./Countries.css";
 
 const Countries = ({ allCountries }) => {
   const allCountry = use(allCountries);
   const countries = allCountry.countries;
+
+  const [totalVisited, setTotalVisited] = useState([]);
+  const handleTotalVisited = () => {
+    const sum = [...totalVisited, countries];
+    setTotalVisited(sum);
+  };
   return (
     <div>
       <h1>Total Countries: {countries.length}</h1>
+      <h2>Total Country Visited: {totalVisited.length} </h2>
       <div className="countries-container">
-      {
-        countries.map(country => <Country key={country.cca3.cca3} country = {country}></Country>)
-      }
+        {countries.map((country) => (
+          <Country
+            key={country.cca3.cca3}
+            country={country}
+            handleTotalVisited={handleTotalVisited}
+          ></Country>
+        ))}
       </div>
     </div>
   );
--- src/Countries/Country.jsx
+++ src/Countries/Country.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import "./Country.css"
+import React, { useState } from "react";
+import "./Country.css";
 
-const Country = ({country}) => {
-    return (
-        <div className="country-card">
-            <img src={country.flags.flags.png} alt="" />
-            <h3>Country Name: {country.name.common}</h3>
-            <p>Population: {country.population.population}</p>
-            <p>Area: {country.area.area} {country.area.area < 300000 ? "Small Country" : "Big Country"}</p>
-            <button>Not Visited</button>
-            <button>Add Visited Flag</button>
-        </div>
-    );
+const Country = ({ country, handleTotalVisited }) => {
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    const clicked = !visited;
+    setVisited(clicked);
+    handleTotalVisited()
+  };
+  
+  return (
+    <div className="country-card">
+      <img src={country.flags.flags.png} alt="" />
+      <h3>Country Name: {country.name.common}</h3>
+      <p>Population: {country.population.population}</p>
+      <p>
+        Area: {country.area.area}{" "}
+        {country.area.area < 300000 ? "(Small Country)" : "(Big Country)"}
+      </p>
+      <button className={visited ? "visited-btn" : ""} onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
+      <button>Add Visited Flag</button>
+    </div>
+  );
 };
 
 export default Country;
